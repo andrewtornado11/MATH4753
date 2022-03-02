14fa89674b591296a88741e6cac4edbe6394c0aa
Andrew Muehr <andrewtornado12@gmail.com>
2022-03-01 20:42:38 -0600

    lab 6

--- R/mybin1.R
+++ R/mybin1.R
@@ -1,15 +1,45 @@
-#' mybin1
+#' @title mybin1
+#' @description function for binomial calculation
 #'
-#' @param y
-#' @param r
-#' @param p
+#' @param y Number of trials
+#' @param r Number of successes
+#' @param p Probability
 #'
 #' @return Probability of < = y
 #' @export
 #'
 #' @examples
+#' mynbin(10,3,0.6)
 mynbin=function(y,r,p){
   choose(y-1,r-1)*p^r*(1-p)^(y-r)
 }
 mynbin(10,3,0.4)
 
+
+#' @title myncurve
+#' @description function for normal curve graph and area calculation
+#'
+#' @param mu mean
+#' @param sigma standard deviation
+#' @param a maximum x-value where probability will be calculated
+#'
+#' @return graph, area shaded in red
+#' @export
+#'
+#' @examples
+#' myncurve(4,3,9)
+myncurve = function(mu, sigma, a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
+
+  xcurve = seq(mu-3*sigma,a,length=100)
+  ycurve = dnorm(xcurve,mu,sigma)
+  polygon(c(mu-3*sigma,xcurve,a),c(0,ycurve,0),col="Red")
+
+  area = pnorm(a,mean=mu,sd=sigma)
+  arear = round(area,4)
+
+  list(arear)
+
+}
+
+myncurve(4,3,9)
