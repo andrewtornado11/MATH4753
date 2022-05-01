5a6feb937d0eb576a25d61e4919ce6290b6df8ff
Andrew Muehr <andrewtornado12@gmail.com>
2022-05-01 17:27:32 -0500

    DDT data set documentation

---
+++ R/DDT.R
@@ -0,0 +1,17 @@
+#' DDT data set for fish
+#'
+#' A dataset containing characteristics (length, width, weight, ddt concentration)
+#' for fish in the Tennessee River and its tributaries
+#'
+#' @format A data set with 144 rows and 5 variables:
+#' \describe{
+#'   \item{LENGTH}{length of fish, in centimeters}
+#'   \item{WEIGHT}{weight of fish, in grams}
+#'   \item{SPECIES}{species of fish}
+#'   \item{MILE}{mile of river at where the fish was found}
+#'   \item{RIVER}{river abbreviation where the fish was found}
+#'   \item{DDT}{concentration of DDT in the fish}
+#'   ...
+#' }
+#' @source Mendenhall and Sincich: Statistics for Engineering and the Sciences, Sixth Edition
+"ddt"
